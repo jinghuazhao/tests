0c95c1c66dc3f8b87df604becf31f9987957b719
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-07-26 10:41:08 +0100

    shinyCircos

--- shinyCircos/utils.sh
+++ shinyCircos/utils.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
+if [ ! -d circos ]; then mkdir circos; fi
+
 for prefix in e p
 do
-  sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTLs.csv > ${prefix}QTLs.txt
-  sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTL_labels.csv > ${prefix}QTL_labels.txt
-  sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTL_colors.csv > ${prefix}QTL_colors.txt
+  sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTLs.csv > circos/${prefix}QTLs.txt
+  sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTL_labels.csv > circos/${prefix}QTL_labels.txt
+  sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTL_colors.csv > circos/${prefix}QTL_colors.txt
 done
