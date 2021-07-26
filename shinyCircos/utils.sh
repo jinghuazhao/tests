198453fa5f463df1ba9fe53aa29e52aa88005205
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-07-26 17:33:10 +0100

    shinyCircos

--- shinyCircos/utils.sh
+++ shinyCircos/utils.sh
@@ -4,27 +4,21 @@ if [ ! -d circos ]; then mkdir circos; fi
 
 for prefix in e p
 do
+  export prefix=${prefix}
   sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTLs.csv > circos/${prefix}QTLs.txt
-  sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTL_labels.csv > circos/${prefix}QTL_labels.txt
+# sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTL_labels.csv > circos/${prefix}QTL_labels.txt
   sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTL_colors.csv > circos/${prefix}QTL_colors.txt
-  (
-    grep -v NA ${prefix}QTL_xlsx.csv | cut -d, -f4,5 | sed '1d;s/,/ /' | \
-    while read gene category
-    do
-       case ${category} in
-         5) color=red;;
-         4) color=blue;;
-         3) color=green;;
-         2) color=yellow;;
-         1) color=purple;;
-         *) echo "nonexistent";;
-       esac
-       echo "<rule>"
-       echo "condition = var(value) eq \"${gene}\""
-       echo "color     = ${color}"
-       echo "</rule>"
-    done
-  ) > circos/${prefix}rule.textcolor.conf
+  awk -v FS="," '$4!="NA" && $5!="NA"' ${prefix}QTL_xlsx.csv | \
+  awk -v FS="," '{
+     colors[1]="dred"
+     colors[2]="dblue"
+     colors[3]="dgreen"
+     colors[4]="dorange"
+     colors[5]="dpurple"
+     sub(/chr/,"hs",$1)
+     if (ENVIRON["prefix"]=="e") label=$4; else label=$4 "----"
+     if (NR>1) print $1,$2-1,$2,label,"color=" colors[$5]
+  }' > circos/${prefix}QTL_labels.txt
 done
 
 sed -i 's/-//' circos/pQTLs.txt
