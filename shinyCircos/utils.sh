d8b2f73c85aa885ad45407b339fa9440b82f3c26
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-07-26 11:15:24 +0100

    shinyCircos

--- shinyCircos/utils.sh
+++ shinyCircos/utils.sh
@@ -7,4 +7,22 @@ do
   sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTLs.csv > circos/${prefix}QTLs.txt
   sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTL_labels.csv > circos/${prefix}QTL_labels.txt
   sed '1d;s/chr/hs/;s/,/ /g' ${prefix}QTL_colors.csv > circos/${prefix}QTL_colors.txt
+  (
+    grep -v NA ${prefix}QTL_xlsx.csv | cut -d, -f4,5 | sed '1d;s/,/ /' | \
+    while read gene category
+    do
+       case ${category} in
+         5) color=red;;
+         4) color=blue;;
+         3) color=green;;
+         2) color=yellow;;
+         1) color=purple;;
+         *) echo "nonexistent";;
+       esac
+       echo "<rule>"
+       echo "condition = var(value) eq \"${gene}\""
+       echo "color     = ${color}"
+       echo "</rule>"
+    done
+  ) > circos/${prefix}rule.textcolor.conf
 done
