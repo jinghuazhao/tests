f313b3b50ed76c8c7f128e697c3306fdd5a58948
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-07-28 16:22:11 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -4,10 +4,12 @@ git add .gitignore
 git commit -m ".gitignore"
 git add README.md
 git commit -m "README.md"
-git add shinyCircos/
-git commit -m "shinyCircos"
+git add anca
+git commit -m "anca"
 git add pan/
 git commit -m "pan"
+git add shinyCircos/
+git commit -m "shinyCircos"
 git add st.sh
 git commit -m "st.sh"
 git push
