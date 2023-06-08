451d3649b331b3b3478a7debe9262cabcadf519f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-06-08 09:20:43 +0100

    ${f}

--- st.sh
+++ st.sh
@@ -1,26 +1,9 @@
 #!/usr/bin/bash
 
-git add .gitignore
-git commit -m ".gitignore"
-git add README.md
-git commit -m "README.md"
-git add METAL
-git commit -m "METAL"
-git add anca
-git commit -m "anca"
-git add locuszoom/
-git commit -m "locuszoom"
-git add gap/
-git commit -m "gap"
-git add pan/
-git commit -m "pan"
-git add shinyCircos/
-git commit -m "shinyCircos"
-git add turboqq
-git commit -m "turboqq"
-git add turboman
-git commit -m "turboman"
-git add st.sh
-git commit -m "st.sh"
+for f in $(ls)
+do
+   git add ${f}
+   git commit -m '${f}'
+done
 git push
 
