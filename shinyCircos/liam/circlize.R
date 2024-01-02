d7bf7fbef8a9ea1b6abb5625ce25f6d3fea556ed
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-01-02 21:25:46 +0000

    shinyCircos

--- shinyCircos/liam/circlize.R
+++ shinyCircos/liam/circlize.R
@@ -8,6 +8,7 @@ sheet1 <- read.xlsx(xlsx,sheet=1,colNames=TRUE,skipEmptyRows=TRUE)
 suppressMessages(library("circlize"))
 suppressMessages(library(dplyr))
 
+cols <- c("black","red")
 QTLs <- rename(sheet1,start=TSS_start) %>%
         mutate(end=start+1) %>%
         arrange(chr,start,end) %>%
@@ -15,7 +16,7 @@ QTLs <- rename(sheet1,start=TSS_start) %>%
         mutate(log10p=if_else(log10p>30,30,log10p)) %>%
         select(chr,start,end,log10p,label,druggability_category)
 QTL_labels <- filter(QTLs,!is.na(druggability_category)) %>%
-              mutate(druggability_category=druggability_category+1)
+              mutate(druggability_category=cols[druggability_category+1])
 QTL_labels
 with(QTL_labels,table(druggability_category))
 
@@ -25,7 +26,7 @@ circos.clear()
 circos.par("start.degree" = 90, gap.degree = c(rep(c(0.7), 21), 8), track.margin = c(0.005, 0.005), cell.padding = c(0.001, 0.01, 0.01, 0.001))
 circos.initializeWithIdeogram(plotType = NULL, species = "hg19",  chromosome.index = paste0("chr", 1:22))
 circos.genomicLabels(QTL_labels, labels=QTL_labels[[5]], side = "outside",
-                     cex = 0.7, line_lwd = 0.7, padding=0.5,
+                     cex = 0.7, font = 4, line_lwd = 0.7, padding=0.5,
                      labels_height=min(c(cm_h(0.5), max(strwidth(QTL_labels, cex = 0.4, font = par("font"))))),
                      connection_height = convert_height(6, "mm"),
                      line_col = QTL_labels[[6]],
@@ -35,7 +36,7 @@ circos.track(ylim = c(0, 1),
                chr  = gsub("chr", CELL_META$sector.index, replace = "")
                xlim = CELL_META$xlim
                ylim = CELL_META$ylim
-               circos.rect(xlim[1], 0, xlim[2], 1, col = "white", cex = 0.2, lwd = 0.5 )
+               circos.rect(xlim[1], 0, xlim[2], 1, col = "white", cex = 0.3, lwd = 0.5 )
                circos.text(mean(xlim), mean(ylim), chr, cex = 0.4, col = "black", facing = "inside", niceFacing = TRUE)
              },
              track.height = 0.03,  bg.border = NA)
@@ -43,13 +44,13 @@ circos.track(ylim=c(0,1), track.height=0.05, bg.border=NA, panel.fun=function(x,
              chr=gsub("chr", CELL_META$sector.index, replace="")
              xlim=CELL_META$xlim
              ylim=CELL_META$ylim
-             circos.genomicAxis(h="top", direction="inside", labels.cex=0.25, major.at=seq(0,1e10,5e7))})
+             circos.genomicAxis(h="top", direction="inside", labels.cex=0.35, major.at=seq(0,1e10,5e7))})
 circos.genomicTrackPlotRegion(QTLs, numeric.column = 4,
                               panel.fun = function(region, value,  ...)
                               circos.genomicPoints(region, value, pch = 16, col = "magenta", cex = 0.3),
                               track.height = 0.55, bg.border = NA, bg.col = "white", ylim = c(0, 30))
 circos.yaxis(side = "left", at = c(0, 10, 20, 30), labels = c(0, 10, 20, 30), 
-             sector.index = get.all.sector.index()[1], labels.cex = 0.3, lwd = 0.3,
+             sector.index = get.all.sector.index()[1], labels.cex = 0.3, lwd = 0.5,
              tick.length = 0.5*(convert_x(1, "mm",
              get.cell.meta.data("sector.index"),
              get.cell.meta.data("track.index"))))
@@ -57,6 +58,7 @@ circos.genomicText(data.frame(start=1,end=1),sector.index=get.all.sector.index()
                    labels = "-log10(P)",
                    h = "bottom",
                    cex = 0.4,
+                   font = 2,
                    y = 20,
                    adj = c(0.2, 1.5),
                    facing = "clockwise")
