f39f2cd490e960bc8b0beceef5238a291b448e80
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2026-06-19 23:35:49 +0100

    gaawr2

--- gaawr2/MCQs.sh
+++ gaawr2/MCQs.sh
@@ -4,3 +4,6 @@ module load ceuadmin/R
 R --no-save < MCQs.R
 module load ceuadmin/pandoc/3.6.2
 pandoc MCQs.md -o MCQs.docx --metadata title=""
+
+# pandoc 2.18 has duplicated titles but normal with --metadata title=""
+# pandoc 3.6.2 may or may not take the option.
